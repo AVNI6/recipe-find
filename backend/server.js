1e6e8a7381d23ba128c987200e41d74f19682254
AVNI6 <avaniradadia@gmail.com>
2025-06-05 21:19:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,7 @@ const recipeRoutes = require('./routes/recipeRoutes');
 const mealPlannerRoutes = require('./routes/mealPlannerRoutes')
 dotenv.config();
 const app = express();
-
-app.use(cors({
-    origin: 'https://recipe-find.onrender.com',
-    credentials: true 
-}));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
